fe119afc2f3e12f9332a1d3685cee04ba9810e61
dittrich, jan <d_jan@ymail.com>
2016-01-23 21:38:58 +0100

    try to get codepen export working...

--- scripts/webedit.js
+++ scripts/webedit.js
@@ -92,34 +92,19 @@ $(function(){
 
 	}
 
-/*	var deleteElement = function(){
-		var canvasSelector = "body";
-		var element2BDeletedSelector = ".custom-selected";
-		$canvas = $(canvasSelector);
-
-		$canvas.find(element2BDeletedSelector).detach();
-	}
-
-	var undeleteElement function(){
-	//reference to former parent
-	//reference to element
-	//reattach to parent
-
-	//or better: but in revealing module; have .delete .undelete
-	}*/
 
 	var deleteElement=(function(){ //revealing module pattern
 		var recentlyDeleted = { //hope saving these does not cause memory leaks. FUD.
 			$element:null,
 			$formerParent:null
-		}
+		};
 		var canvasSelector = "body";
 		var element2BDeletedSelector = ".custom-selected";
-		console.log("init deleteElement")
+
 		return{
 			delete:function(){
-				$canvas = $(canvasSelector);
-				$element2BDeleted = $canvas.find(element2BDeletedSelector);
+				var $canvas = $(canvasSelector);
+				var $element2BDeleted = $canvas.find(element2BDeletedSelector);
 
 				recentlyDeleted.$formerParent = $element2BDeleted.parent(); //remember parent for re-attachment on redo
 				recentlyDeleted.$element = $element2BDeleted.detach(); //delete element and store it  for re-attachment on redo
@@ -128,15 +113,18 @@ $(function(){
 			undelete:function(){
 
 				if(!recentlyDeleted.$element || !recentlyDeleted.$formerParent ){
-					return
+					return;
 				}
 
 				recentlyDeleted.$formerParent.append(recentlyDeleted.$element);
 			}
-		}
-	}())
+		};
+	}());
 
-	var setupGUI = function(){
+
+(function(){ //setup the gui
+
+		/*a dialog to change the canvas size. Maybe a bit over the top to use jqui for that.*/
 		$("#changeCanvasSizeDialog").dialog({
 			autoOpen:false,
 			open: function( event, ui ) {
@@ -215,9 +203,60 @@ $(function(){
 		window.onbeforeunload = function(){
 			return "do you want to close the application? Unsaved changes will be lost (use your browsers save function for saving)"
 		};
-	};
 
-	setupGUI();
+		$("<button>to Codepen</button>").appendTo("#toolbar").click(function(){
+		var htmlSource = $("#canvasWrap"),
+			cssSource = $("#elementStyles"),
+			cleanThese=".ui-resizable-handle";
+
+		//if there is already a send-to-codepen-form, delete it
+		$("form#sendToCodepen").remove();
+
+		var htmlString = htmlSource.
+			clone(). //for coming manipulations, so we don't actually change the original
+			find(".ui-resizable-handle"). //find handles...
+			remove(). //remove them (not useful when displaying)
+			end(). //go out of the matched handles (via find) the the previous set (all in htmlSource)
+			find("*").//every element
+			each(function(index,element){
+				$(element).
+				contents(). //all subnodes of this element, including Text nodes
+				filter(function() {
+					return this.nodeType === 3; //filter for text nodes
+				}).each(function(index,element){ //for each text node
+					var oldtext = element.nodeValue;
+					element.nodeValue= oldtext.replace(/"/g, "&Prime;").replace(/'/g, "&prime;"); //replace all " by hour sign entity, all ' by minute sign entity. Why not their respective entities (the ones used here only look similar)? They will be replaced later by another function and seemingly these signs mess up the validity of the html, since once they are stringified, there is no distinction between markup chars and text-content chars anymore.
+				});
+			}).
+			html();
+			console.log(htmlString)
+
+		var cssString = cssSource
+			.html();
+
+		var data={
+			html:htmlString,
+			css:cssString,
+			js:""
+		}
+
+		var jsonstring = JSON.stringify(data).replace(/"/g, "&quot;").replace(/'/g, "&apos;");
+
+		var form =
+	'<form id="sendToCodepen" action="http://codepen.io/pen/define" method="POST" target="_blank" style="position:fixed; top:0;left:0;">' +
+		'<input type="hidden" name="data" value=\'' +
+			jsonstring +
+			'\'>' +
+		'<input type="image" src="http://s.cdpn.io/3/cp-arrow-right.svg" width="40" height="40" value="Create New Pen with Prefilled Data" class="codepen-mover-button">' +
+	'</form>';
+
+			$(form).appendTo("body")
+		});
+
+
+
+	})();//setupgui end
+
 
 	//setup elements already on the page
 	$(".mockElement").each(function(index, element){
