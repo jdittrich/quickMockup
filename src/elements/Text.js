8c1f99c84a7be92e45db407c457274d2f2709b31
Alaa Sarhan <sarhan.alaa@protonmail.com>
2019-12-08 23:15:06 +0100

    Improvements to Text element - default width of 100 - remove stroke

--- src/elements/Text.js
+++ src/elements/Text.js
@@ -4,6 +4,7 @@ const textEditor = new MultilineInplaceEditor()
 
 function Text({
     text = 'This is text',
+    width = 100,
     ...props
 } = {}) {
     this.name = 'quickMockup.Text'
@@ -11,11 +12,13 @@ function Text({
     this.figure = new draw2d.shape.basic.Text({
         bgColor: 'transparent',
         color: 'black',
-        resizeable: true,
         padding: { left: 0, right: 0 },
+        stroke: 0,
         text,
         ...props
     })
+
+    this.figure.setDimension(width)
 
     this.figure.on('dblclick', () => { textEditor.start(this.figure) })
 }
