884a100ac55edf0df62b84688a38823d6856c65f
Alaa Sarhan <sarhan.alaa@protonmail.com>
2019-12-13 18:42:50 +0100

    Attempt 1: use dragenter, dragleave and move events to implement potential container hint effect
    This solution seems to work as a user would expect in all cases. There's only one
    visual caveat when a figure, that is already inside a container that is inside
    another container, is dragged. In that case, only most inner-most container will glow.
    That contradicts the visual hinting when the user might drag another figure from outside
    both containers into the inner container, on which event that outer container will
    glow too.

--- src/shape/composite/ContainerComposite.js
+++ src/shape/composite/ContainerComposite.js
@@ -1,4 +1,11 @@
+let enteredContainers = []
+
 const Container = draw2d.shape.composite.StrongComposite.extend({
+    init: function(...args) {
+        this._super(...args)
+        this.isPotentialContainer = false
+    },
+
     assignFigure: function (figure) {
         this.assignedFigures.add(figure)
         figure.setComposite(this)
@@ -11,11 +18,54 @@ const Container = draw2d.shape.composite.StrongComposite.extend({
         return this
     },
 
+    onEnteredFigureMove: function (figure) {
+        // Due to throttling this hanlder, a call might happen after
+        // drag has ended, in which case we do not want to do anything
+        if (!figure.isInDragDrop) {
+            return
+        }
+        // when dragged element moves entirely inside the container,
+        // we want to mark the container as potential container for that
+        // figure
+        if (this.getBoundingBox().contains(figure.getBoundingBox())) {
+            this.setGlow(true)
+            enteredContainers.push(this)
+        } else {
+            // otherwise we mark this container back as no potential container
+            this.setGlow(false)
+        }
+    },
+
+    onDragEnter: function(figure) {
+        // start tracking the movement of dragged element
+        this.throttledMoveHandler && figure.off(this.throttledMoveHandler)
+        this.throttledMoveHandler = $.throttle(100, (canvas, { figure }) => {
+            this.onEnteredFigureMove(figure)
+        })
+        figure.on('move', this.throttledMoveHandler)
+    },
+
     onDragLeave: function(figure) {
+        this.throttledMoveHandler && figure.off(this.throttledMoveHandler)
+
+        // when a dragged figure enters another container that is inside the
+        // boundaries of this container, then a drag leave and drag enter
+        // events will be fired on this container consequently.
+        // To avoid flickering glow triggering on this container, do not
+        // turn off glow if dragged figure is still inside the boundaries
+        // of this container
+        if (!this.getBoundingBox().contains(figure.getBoundingBox())) {
+            this.setGlow(false)
+        }
         this.unassignFigure(figure)
     },
 
     onCatch: function (figure) {
+        this.setGlow(false)
+        this.throttledMoveHandler && figure.off(this.throttledMoveHandler)
+        enteredContainers.forEach(c => c.setGlow(false))
+        enteredContainers = []
+
         if (figure.getComposite()) {
             return
         }
@@ -47,6 +97,16 @@ const Container = draw2d.shape.composite.StrongComposite.extend({
 
     toFront: function(figure) {
         this._super(figure)
+    },
+
+    setGlow: function (glow) {
+        if (glow && !this.originalStroke) {
+            this.originalStroke = this.getStroke()
+            this.setStroke( this.originalStroke + 1)
+        } else if (!glow && this.originalStroke) {
+            this.setStroke(this.originalStroke)
+            delete this.originalStroke
+        }
     }
 
 })
